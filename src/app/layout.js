691949332f8ddf8e95d90bf875eb985a4ed51fba
abusayeds <abusayedstudent855@gmail.com>
2026-05-05 11:03:27 +0600

    feat(app): add root layout and configurations

---
+++ src/app/layout.js
@@ -0,0 +1,39 @@
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { Poppins } from "next/font/google";
+import Navbar from "../components/Navbar/Navbar";
+import { SocketProvider } from "../context/SocketContext";
+import ThemeProvider from "../lib/ThemeProvider";
+import "./globals.css";
+import StoreProvider from "./StoreProvider";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-poppins",
+});
+
+export const metadata = {
+  title: "Peared",
+  description: "Service Provider Web App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased bg-white`}>
+        <SocketProvider>
+          <StoreProvider>
+            <Navbar />
+            <AntdRegistry>
+              <ThemeProvider>
+                <div className="pt-20">{children}</div>
+              </ThemeProvider>
+            </AntdRegistry>
+            {/* <Footer /> */}
+            {/* <NotificationListener /> */}
+          </StoreProvider>
+        </SocketProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,27 @@
+"use client";
+
+import { useSelector } from "react-redux";
+import BottomBar from "../components/BottomBar/BottomBar";
+import Footer from "../components/Footer/Footer";
+import Banner from "../components/Home/Banner/Banner";
+import Feedback from "../components/Home/Feedback/Feedback";
+import PopularServices from "../components/Home/PopularServices/PopularServices";
+import WantContractor from "../components/Home/WantContractor/WantContractor";
+import WhatPeared from "../components/Home/WhatPeared/WhatPeared";
+import WhyPeared from "../components/Home/WhyPeared/WhyPeared";
+
+export default function Home() {
+  const { user } = useSelector((state) => state.auth);
+  return (
+    <>
+      <Banner />
+      <PopularServices />
+      <WhatPeared />
+      <WhyPeared />
+      <Feedback />
+      <WantContractor />
+      <Footer />
+      {user && <BottomBar />}
+    </>
+  );
+}
